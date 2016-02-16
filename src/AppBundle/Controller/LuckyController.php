0f93f90e304c9fc29b794cf8f2ede5b82c32633e
miholeus <me@miholeus.com>
2016-02-17 01:42:05 +0300

    start

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,36 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+  /**
+   * @Route("/lucky/number/{count}")
+   */
+  public function numberAction($count)
+  {
+    $numbers = [];
+    for ($i=0;$i<$count;$i++) {
+      $numbers[] = rand(0, 1000);
+    }
+    $number = implode(",", $numbers);
+    $html = $this->container->get('templating')->render(
+      'lucky/number.html.twig',
+      ['numberList' => $number]
+    );
+    return new Response($html);
+  }
+  /**
+   * @Route("/api/lucky/number")
+   */
+  public function apiNumberAction()
+  {
+    $data = ['lucky_number' => rand(0, 1000)];
+
+    return new JsonResponse($data);
+  }
+}
