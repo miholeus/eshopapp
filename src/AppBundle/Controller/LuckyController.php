66b4565c4c897b1ac40ee00cb7821ac4c3bb156b
miholeus <me@miholeus.com>
2016-02-19 02:51:59 +0300

    session

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -3,6 +3,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,12 +12,15 @@ class LuckyController extends Controller
   /**
    * @Route("/lucky/number/{count}")
    */
-  public function numberAction($count)
+  public function numberAction($count, Request $request)
   {
     $numbers = [];
     for ($i=0;$i<$count;$i++) {
       $numbers[] = rand(0, 1000);
     }
+    $session = $request->getSession();
+    $session->set('number', rand(0, 100));
+
     $number = implode(",", $numbers);
     $html = $this->container->get('templating')->render(
       'lucky/number.html.twig',
