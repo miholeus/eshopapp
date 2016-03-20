b8efaa94ddc4f1bd9b91932d8cf1c3238e08dfc0
miholeus <me@miholeus.com>
2016-03-20 21:41:33 +0300

    twitter bootstrap installed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,47 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository('AppBundle:Product')
+            ->findAllOrderedByName();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'products' => $products
         ));
+    }
+
+    /**
+     * @Route("/create/", name="createproduct")
+     * @return Response
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription("Some test description");
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+
+        return new Response("Created Product by id: " . $product->getId());
+    }
+
+    /**
+     * @param $id
+     * @return Response
+     */
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()->getRepository('AppBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id ' . $id);
+        }
+
+        return $this->render(':product:show.html.twig', ['product' => $product]);
     }
 }
